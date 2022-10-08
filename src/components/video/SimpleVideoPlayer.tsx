b6698fda5a7dce0f162f3d3d45aac915f4673e9f
Hyunsub Kim <embrapers263@gmail.com>
2022-10-08 20:43:29 +0900

    Reload video element in iOS device when video url changed

--- src/components/video/SimpleVideoPlayer.tsx
+++ src/components/video/SimpleVideoPlayer.tsx
@@ -1,13 +1,20 @@
+import { useEffect, useRef } from 'react';
 import { VidepPlayerProps } from './VideoPlayer';
 
 export default function SimpleVideoPlayer({ thumbnailUrl, videoUrl, subtitles }: VidepPlayerProps) {
+  const ref = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    ref.current?.load();
+  }, [videoUrl])
+
   const tracks = subtitles.map((v) => (
     <track key={v.url} kind="captions" label={v.label} srcLang={v.srclang} src={v.url} />
-  ))
+  ));
 
   return (
     <div className="ratio ratio-16x9">
-      <video id="player" playsInline controls crossOrigin="use-credentials" poster={thumbnailUrl}>
+      <video ref={ref} id="player" playsInline controls crossOrigin="use-credentials" poster={thumbnailUrl}>
         <source src={videoUrl} type="video/mp4" />
         {tracks}
       </video>
