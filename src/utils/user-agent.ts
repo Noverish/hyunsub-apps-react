b31b51b8e220ec3bde981ae6754b0c292417f6de
Hyunsub Kim <embrapers263@gmail.com>
2022-12-09 17:50:39 +0900

    isMobile

--- src/utils/user-agent.ts
+++ src/utils/user-agent.ts
@@ -1,4 +1,4 @@
-export const isMobile = () => navigator.userAgent.indexOf('Mobi') !== -1;
+export const isMobile = () => isIOS() || isAOS();
 export const isIOS = () =>
   ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform)
   || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
