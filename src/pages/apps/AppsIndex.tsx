6c5ed1274b1f6d4da4c789fe9c024fa524962b5c
Hyunsub Kim <embrapers263@gmail.com>
2023-01-15 10:49:34 +0900

    apps use authorities

--- src/pages/apps/AppsIndex.tsx
+++ src/pages/apps/AppsIndex.tsx
@@ -1,77 +1,69 @@
 import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import getAuthorities from "src/api/auth/authorities";
 
 import './AppsIndex.scss';
 
-export default function AppsIndex() {
-  const [click, setClick] = useState(0);
+interface Service {
+  name: string;
+  icon: string;
+  title: string;
+}
 
-  const onClick = () => {
-    if (click < 4) {
-      setClick(v => v + 1);
-    } else {
-      window.location.href = 'https://agnam.hyunsub.kim'
-    }
-
-    setTimeout(() => {
-      setClick(0);
-    }, 3000);
+const services: Service[] = [
+  {
+    name: 'video',
+    icon: 'fas fa-film',
+    title: 'Video',
+  },
+  {
+    name: 'photo',
+    icon: 'fas fa-camera',
+    title: 'Photo',
+  },
+  {
+    name: 'comic',
+    icon: 'fas fa-book',
+    title: 'Comic',
+  },
+  {
+    name: 'apparel',
+    icon: 'fas fa-tshirt',
+    title: 'Apparel',
+  },
+  {
+    name: 'drive',
+    icon: 'fas fa-hdd',
+    title: 'Drive',
+  },
+  {
+    name: 'agnam',
+    icon: 'fas fa-torii-gate',
+    title: 'Agnam',
   }
+]
+
+export default function AppsIndex() {
+  document.title = 'Hyunsub Apps';
+
+  const authorities = getAuthorities.useApi({});
+
+  const items = services.filter(v => authorities.includes(v.name));
+
+  const elements = items.map(v => (
+    <Col key={v.name}>
+      <a href={`https://${v.name}.hyunsub.kim`}>
+        <i className={v.icon}></i>
+        <span>{v.title}</span>
+      </a>
+    </Col>
+  ))
 
   return (
-    <div id="AppsIndex" onClick={onClick}>
+    <div id="AppsIndex">
       <div className="wrapper">
-        <Row>
-          <Col>
-            <a href="https://video.hyunsub.kim">
-              <i className="fas fa-film"></i>
-              <span>Video</span>
-            </a>
-          </Col>
-          <Col>
-            <a href="https://photo.hyunsub.kim">
-              <i className="fas fa-camera"></i>
-              <span>Photo</span>
-            </a>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <a href="https://apparel.hyunsub.kim">
-              <i className="fas fa-tshirt"></i>
-              <span>Apparel</span>
-            </a>
-          </Col>
-          <Col>
-            <a href="https://drive.hyunsub.kim">
-              <i className="fas fa-hdd"></i>
-              <span>Drive</span>
-            </a>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <a href="https://archive.hyunsub.kim">
-              <i className="fas fa-archive"></i>
-              <span>Archive</span>
-            </a>
-          </Col>
-          <Col>
-            <a href="https://git.hyunsub.kim">
-              <i className="fab fa-git-alt"></i>
-              <span>Git</span>
-            </a>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <a href="https://comic.hyunsub.kim">
-              <i className="fas fa-book"></i>
-              <span>Comic</span>
-            </a>
-          </Col>
-          <Col>
-          </Col>
+        <Row className="row-cols-2 g-5">
+          {elements}
         </Row>
       </div>
     </div>
