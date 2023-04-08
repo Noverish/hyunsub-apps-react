1d4e96a0db58a13a2e5d6ecd805d4a3a211bfadf
Hyunsub Kim <embrapers263@gmail.com>
2023-04-08 20:36:13 +0900

    AlbumListPage PC album create button

--- src/pages/photo/album-list-2/AlbumListPage.tsx
+++ src/pages/photo/album-list-2/AlbumListPage.tsx
@@ -1,4 +1,4 @@
-import { Row } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import albumListV2Api from "src/api/photo/album-list-v2"
 import CommonContainer from "src/components/common/header/CommonContainer";
 import MobileHeader from "src/components/common/header/MobileHeader";
@@ -12,6 +12,7 @@ import { useContext } from "react";
 import AlbumCreateModal from "src/components/photo/modal/AlbumCreateModal";
 import { AlbumCreateParams } from "src/api/photo/album-create";
 import { useAlbumCreate } from "./AlbumListHooks";
+import { useBreakpointMobile } from "src/utils/breakpoint";
 
 function AlbumListPage() {
   setDocumentTitle(t('photo.page.album-list.title'));
@@ -20,6 +21,7 @@ function AlbumListPage() {
   const [state, setState] = useContext(AlbumListContext);
   const albums = albumListV2Api.useApi({});
   const albumCreate = useAlbumCreate();
+  const isMobile = useBreakpointMobile();
 
   // functions
   const albumCreateModalCallback = async (params?: AlbumCreateParams) => {
@@ -29,6 +31,8 @@ function AlbumListPage() {
     setState({ showAlbumCreateModal: false });
   }
 
+  const showAlbumCreateModal = () => setState({ showAlbumCreateModal: true });
+
   // elements
   const elements = albums.map(v => (
     <AlbumPreviewView key={v.id} preview={v} onClick={PhotoRoutes.albumDetail2(v.id)} />
@@ -37,14 +41,21 @@ function AlbumListPage() {
   const headerBtns: MobileHeaderButton[] = [
     {
       icon: 'fas fa-plus',
-      onClick: () => setState({ showAlbumCreateModal: true }),
+      onClick: showAlbumCreateModal,
     }
   ]
+
+  const topBtns = (
+    <div className="mb-3">
+      <Button onClick={showAlbumCreateModal}>{t('AlbumCreateModal.title')}</Button>
+    </div>
+  )
 
   return (
     <div className="AlbumList2Page">
       <MobileHeader title={t('photo.page.album-list.title')} btns={headerBtns} />
       <CommonContainer>
+        {isMobile || topBtns}
         <Row className="g-2 row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6">
           {elements}
         </Row>
