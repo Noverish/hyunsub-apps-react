d71c9b03110acb6e940797def518f20d2f67b80a
Hyunsub Kim <embrapers263@gmail.com>
2023-05-17 01:16:10 +0900

    [video] pagination bug fixed

--- src/components/common/CommonPagination.tsx
+++ src/components/common/CommonPagination.tsx
@@ -24,7 +24,7 @@ export default function CommonPagination({ now, total, onClick }: PaginationProp
     </Pagination.Item>
   ));
 
-  if (total <= size) {
+  if (total <= 1) {
     return <></>
   }
 
