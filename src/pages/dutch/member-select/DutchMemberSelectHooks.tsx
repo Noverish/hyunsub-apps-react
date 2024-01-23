47193d86540bc08058d27278c27801cb87ed692a
Hyunsub Kim <embrapers263@gmail.com>
2024-01-23 20:54:17 +0900

    [dutch] local storage test

--- src/pages/dutch/member-select/DutchMemberSelectHooks.tsx
+++ src/pages/dutch/member-select/DutchMemberSelectHooks.tsx
@@ -14,13 +14,21 @@ function useSelect() {
   const { tripId } = usePageParams();
 
   return (member: DutchMember) => {
+    window.localStorage.setItem(tripId, member.id);
     window.location.href = `/bridge/entry/${tripId}/member?memberId=${member.id}`;
   };
+}
+
+function useMemberId() {
+  const { tripId } = usePageParams();
+
+  return window.localStorage.getItem(tripId);
 }
 
 const DutchMemberSelectHooks = {
   usePageParams,
   useSelect,
+  useMemberId,
 };
 
 export default DutchMemberSelectHooks;
